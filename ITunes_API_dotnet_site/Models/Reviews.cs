b061fd7adaa3483fb396ac0b9872e06735374450
Alex Seufert <amseufert@gmail.com>
2020-08-23 22:10:48 -0400

    misc final updates

---
+++ ITunes_API_dotnet_site/Models/Reviews.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace ITunes_API_dotnet_site.Models
+{
+    public class Review
+    {
+        [Key]
+        public int reviewId { get; set; }
+        [Range(0,5)]
+        public int rating { get; set; }
+        public string username { get; set; }
+        public string title { get; set; }
+        public string review { get; set; }
+        [NotMapped]
+        public int formId { get; set; }
+        [JsonIgnore]
+        public Application app { get; set; }
+    }
+}
